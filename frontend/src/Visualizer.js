6f3f13eda68cf0d1748ef4da1b9ebb8cbae443a2
Robert Bieber <robby@bieberphoto.com>
2021-06-27 15:38:06 -0400

    Added Konva Canvas
    I stuck a dummy Konva canvas into the layout with just a garish
    background and a draggable rectangle.  The main thing here is getting
    the resizing logic working right, now that that's in place I'll render
    the board next.

--- frontend/src/Visualizer.js
+++ frontend/src/Visualizer.js
@@ -1,7 +1,54 @@
+import {useRef, useState, useLayoutEffect} from 'react';
+import {Stage, Layer, Rect} from 'react-konva';
+
+function useSize(target) {
+	const [size, setSize] = useState();
+	useLayoutEffect(
+		() =>  setSize(
+			target.current && target.current.getBoundingClientRect(),
+		),
+		[target],
+	);
+	useLayoutEffect(
+		() => {
+			const elem = target.current;
+			if (!elem) {
+				return;
+			}
+
+			const resizeObserver = new ResizeObserver(
+				() => setSize(elem.getBoundingClientRect()),
+			);
+			resizeObserver.observe(elem);
+
+			return () => resizeObserver.unobserve(elem);
+		},
+		[target],
+	);
+
+	return size;
+}
+
+
 export default function Visualizer() {
+	const target = useRef(null);
+	const size = useSize(target);
+
+
+	let stage = null;
+	if (size) {
+		stage = (
+			<Stage width={size.width} height={size.height} fill="red">
+				<Layer>
+					<Rect width={100} height={100} fill="green" draggable />
+				</Layer>
+			</Stage>
+		);
+	}
+
 	return (
-		<div className="Visualizer">
-			Test
+		<div ref={target} className="Visualizer">
+			{stage}
 		</div>
 	);
 }
